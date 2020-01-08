ecf4d9794b7ad8d6cdd09e4498cc640542c14c16
Karol Lobos <karollobos@interia.pl>
2020-01-08 20:23:18 +0100

    Add button to script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+function Button(text) {
+    this.text = text || 'Hello!';
+}
+
+Button.prototype = {
+    create: function() {
+        var self = this;
+        this.element = document.createElement('button');
+        this.element.innerText = this.text;
+        this.element.addEventListener('click', function() {
+            alert(self.text);
+        });
+        document.body.appendChild(this.element);
+    }
+}
+
+var button = new Button('Guten morgen!');
+button.create();
